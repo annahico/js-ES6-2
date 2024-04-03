52ab0856c48aad34c54a92735f54661109f3edea
annahico <annahico@gmail.com>
2024-04-03 20:36:50 +0200

    practicando for of

---
+++ js/09.js
@@ -0,0 +1,16 @@
+//for of
+
+let apellidos = ["perez", "garcia", "gomez"];
+
+// for (let setname of apellidos) {
+//     console.log(setname);
+// }
+
+for (let i = 0; i < apellidos.length; i++) {
+    console.log(apellidos[i]);
+}
+
+for (let apellido of apellidos) {
+    console.log(apellido);
+    
+}
